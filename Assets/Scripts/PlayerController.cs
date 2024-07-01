6ae15d9018715c4ae9c7b73cf41a948528b350c3
JaeYoun Kim <rlawodusx@naver.com>
2024-07-01 21:17:13 +0900

    Feat: 캐릭터 걷는 애니메이션 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
 
-   
+    Animator animator; // 애니메이터 조작을 위한 변수 
 
     [SerializeField] private float moveSpeed = 3.0f;    // 기본 이동 속도 
-    [SerializeField] private float jumpForce = 7.0f;    // 점프 힘 
+    [SerializeField] private float jumpForce = 5.0f;    // 점프 힘
 
 
     private void Awake() {
         rigid2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -32,16 +33,18 @@ public class PlayerController : MonoBehaviour
         // x축은 : Input 좌표 * 설정한 이동속도 , y축은 기본 속력(현재는 RigidBody 적용으로 중력) 
         rigid2D.velocity = new Vector2(x * moveSpeed, rigid2D.velocity.y);
 
+        // 걷는 애니메이션 처리 
+        if(Mathf.Abs(rigid2D.velocity.x) < 0.3) {
+            animator.SetBool("isWalking", false);
+        } else {
+            animator.SetBool("isWalking", true);
+        }
+
     }
 
     // 플레이어의 점프 처리 
     public void Jump() {
         // JumpForce 만큼 위쪽으로 속력을 설정 
         rigid2D.velocity = Vector2.up * jumpForce;
-    }
-
-    // 플레이어 방향키 입력에 따른 좌우 전환 로직 
-    public void Flip() {
-
     }
 }
