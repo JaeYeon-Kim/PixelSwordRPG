d77d708b069c97b8a797096acd29460a90dcd7d8
JaeYoun Kim <rlawodusx@naver.com>
2024-07-08 19:08:15 +0900

    Feat: 게임 메인 UI 추가

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainUi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // 게임 시작 
+    public void OnClickNewGame() {
+        Debug.Log("게임시작!!");
+        mainUi.gameObject.SetActive(false);
+    }
+
+    // 옵션 설정
+    public void OnClickOptionMenu() {
+        Debug.Log("옵션켜기!!");
+    } 
+}
